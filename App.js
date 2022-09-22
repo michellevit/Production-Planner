13c5404c7c319350ef214a0bc92f8b7ae642ce81
Michelle Flandin <mvflandin@gmail.com>
2022-09-21 19:45:02 -0700

    Add file reader feature

---
+++ App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./App.css";
+import * as XLSX from "xlsx";
+
+function App() {
+  const readExcel = (file) => {
+    const promise = new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+      fileReader.readAsArrayBuffer(file);
+      fileReader.onload = (e) => {
+        const bufferArray = e.target.result;
+        const wb = XLSX.read(bufferArray, { type: "buffer" });
+        const wsname = wb.SheetNames[1];
+        const ws = wb.Sheets[wsname];
+        const data = XLSX.utils.sheet_to_json(ws);
+        resolve(data);
+      };
+      fileReader.onerror = (error) => {
+        reject(error);
+      };
+    });
+    promise.then((d) => {
+      console.log("Data: ", d);
+    });
+  };
+
+  return (
+    <div>
+      <input
+        type="file"
+        onChange={(e) => {
+          const file = e.target.files[0];
+          readExcel(file);
+        }}
+      />
+    </div>
+  );
+}
+
+export default App;
