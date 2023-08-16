8d553d289d48316e2cd6738682a4fcb908eb00c1
michellevit <mvflandin@gmail.com>
2023-08-16 13:55:35 -0700

    Add current date to page and minor CSS changes

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,16 +1,23 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 //Importing Components
 import Order from "./components/pages/openOrdersPage/Order";
 
 function App() {
+  const [currentDate, setCurrentDate] = useState("");
+  useEffect(() => {
+    const options = { weekday: "long", month: "long", day: "numeric" };
+    const formattedDate = new Date().toLocaleDateString(undefined, options);
+    setCurrentDate(formattedDate);
+  }, []);
   return (
     <div className="App">
       <header>
         <h1>Open Orders</h1>
       </header>
-      <h2>Date: </h2>
+      <h2>{currentDate}</h2>
       <Order />
     </div>
   );
