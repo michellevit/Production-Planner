478ce6e653371b121a54846c077d795ad369cc63
Michelle <mvflandin@gmail.com>
2023-11-17 10:28:25 -0800

    Update readme with .env instructions

--- start-app.bat
+++ start-app.bat
@@ -22,7 +22,10 @@ docker-compose up -d
 
 REM Print a message indicating the application is running
 echo Backend started and running on http://localhost:8000/
-echo Frontend started and running on http://localhost
+echo Frontend started and running on http://localhost:3000/
 
-REM You can add more commands here, such as logging or notifications
-pause
+REM Automatically open the default web browser at localhost:3000
+start http://localhost:3000/
+
+REM pause keeps the terminal window open after deploying the app (currently commented out)
+REM pause
