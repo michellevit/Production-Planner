a385680d3c3610e7ca6a18183e552fa68876afa9
unknown <Michelle Flandin@GTC.local>
2024-01-09 23:48:07 -0800

    Update README

--- scripts/add-data-to-db.bat
+++ scripts/add-data-to-db.bat
@@ -4,9 +4,6 @@ cd C:\Users\Michelle Flandin\Documents\Coding_Projects\Production-Planner
 :: Activate the virtual environment
 call powershell -NoProfile -ExecutionPolicy Bypass -Command ".\venv\Scripts\Activate.ps1"
 
-@REM for first time app deployment: pip install django
-
-:: Run script
 python .\django\api\scripts\qb_data_to_db.py
 
 pause
