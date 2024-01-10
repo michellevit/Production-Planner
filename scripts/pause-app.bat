97af1509a38c2320150b1613d3086517285ca3c4
unknown <Michelle Flandin@GTC.local>
2024-01-09 23:06:40 -0800

    Create .bat file to add data to db

--- scripts/pause-app.bat
+++ scripts/pause-app.bat
@@ -1,13 +1,13 @@
 @echo off
 
-REM Change directory to where your docker-compose.yml file is located
+:: Change directory to where your docker-compose.yml file is located
 cd C:\Users\Michelle Flandin\Documents\Coding_Projects\Production-Planner
 
-REM Pause the Docker Compose application
+:: Pause the Docker Compose application
 docker-compose pause
 
-REM Print a message indicating the application has been paused
+:: Print a message indicating the application has been paused
 echo The Production Planner application has been paused.
 
-REM pause keeps the terminal window open after pausing the app (optional)
+:: pause keeps the terminal window open after pausing the app (optional)
 pause
--- scripts/start-app.bat
+++ scripts/start-app.bat
@@ -1,17 +1,17 @@
 @echo off
 
-REM Change directory to where your docker-compose.yml file is located
+:: Change directory to where your docker-compose.yml file is located
 cd C:\Users\Michelle Flandin\Documents\Coding_Projects\Production-Planner
 
-REM Start your Docker Compose application
+:: Start your Docker Compose application
 docker-compose up -d
 
-REM Print a message indicating the application is running
+:: Print a message indicating the application is running
 echo Backend started and running on http://localhost:8000/
 echo Frontend started and running on http://localhost:3000/
 
-REM Automatically open the default web browser at localhost:3000
+:: Automatically open the default web browser at localhost:3000
 start http://localhost:3000/
 
-REM pause keeps the terminal window open after deploying the app (optional)
-REM pause
+:: pause keeps the terminal window open after deploying the app (optional)
+:: pause
