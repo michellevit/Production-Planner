b63e273251d5fc10852d843c800533ed4ec75417
unknown <Michelle Flandin@GTC.local>
2024-01-16 20:08:52 -0800

    Fix automated send email functionality

--- scripts/error_scripts/send_critical_error_email.py
+++ scripts/error_scripts/send_critical_error_email.py
@@ -7,6 +7,7 @@ from email.mime.multipart import MIMEMultipart
 import ssl
 from datetime import datetime
 import pytz
+import time
 
 
 load_dotenv()
@@ -29,7 +30,7 @@ def set_custom_message(error_source):
     elif error_source == "Unknown":
         with open('error-log-file.txt', 'r') as log_file:
             error_log_contents = log_file.read()
-        return "An unknown error has occurred with the Production Planner backup and/or Application run process.\n\nError Log:\n{error_log_contents}"
+        return f"An unknown error has occurred with the Production Planner backup and/or Application run process.\n\nError Log:\n{error_log_contents}"
 
 
 def send_email(custom_message):
@@ -43,17 +44,19 @@ def send_email(custom_message):
     msg['From'] = sender_email
     msg['To'] = receiver_email
     msg['Subject'] = 'Production Planner - Error Alert' 
-
     msg.attach(MIMEText(custom_message, 'plain'))
-
     try:
         with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
             server.login(sender_email, password)
             result = server.sendmail(sender_email, receiver_email, msg.as_string())
             if result:
                 print(f"Failed to send email to: {result}")
+                sys.exit(1)
+            else:
+                sys.exit(0)
     except Exception as e:
         print(f"An error occurred: {e}")
+        sys.exit(1)  
 
 
 if __name__ == "__main__":
@@ -62,5 +65,4 @@ if __name__ == "__main__":
     else:
         error_source = "Unknown"
     custom_message = set_custom_message(error_source)
-    print(custom_message)
     send_email(custom_message)
